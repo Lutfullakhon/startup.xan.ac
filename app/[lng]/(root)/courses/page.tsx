d122447813c18756faa0a156643dd225c55f03c8
Lutfullakhon <lutfullaxona@gmail.com>
2025-08-02 22:05:12 +0500

    #updated

--- app/[lng]/(root)/courses/page.tsx
+++ app/[lng]/(root)/courses/page.tsx
@@ -3,11 +3,25 @@ import AllCourses from './_components/all-courses'
 import { getAllCourses } from '@/actions/course.action'
 import { SearchParamsProps } from '@/app.types'
 
-async function Page({ searchParams }: SearchParamsProps) {
+interface PageProps {
+	searchParams: { [key: string]: string | string[] | undefined }
+}
+
+async function Page({ searchParams }: PageProps) {
+	const searchQuery = Array.isArray(searchParams.q)
+		? searchParams.q[0]
+		: searchParams.q
+	const filter = Array.isArray(searchParams.filter)
+		? searchParams.filter[0]
+		: searchParams.filter
+	const page = Array.isArray(searchParams.page)
+		? parseInt(searchParams.page[0])
+		: parseInt(searchParams.page || '1')
+
 	const resultJSON = await getAllCourses({
-		searchQuery: searchParams.q,
-		filter: searchParams.filter,
-		page: searchParams.page ? +searchParams.page : 1,
+		searchQuery,
+		filter,
+		page,
 	})
 
 	const result = JSON.parse(JSON.stringify(resultJSON))
