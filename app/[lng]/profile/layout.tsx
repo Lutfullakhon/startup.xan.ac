06e7deb14e0cb081b9544dbb1132231bd718d251
Lutfullakhon <lutfullaxona@gmail.com>
2025-08-14 23:26:30 +0500

    optimazition2

--- app/[lng]/profile/layout.tsx
+++ app/[lng]/profile/layout.tsx
@@ -6,7 +6,13 @@ function Layout({ children }: ChildProps) {
 	return (
 		<>
 			<Navbar isProfile />
-			<Sidebar page='user' />
+			<Sidebar
+				page='user'
+				isOpen={false}
+				closeSidebar={function (): void {
+					throw new Error('Function not implemented.')
+				}}
+			/>
 			<main className='w-full p-4 pl-[320px] pt-[12vh] max-md:pl-20'>
 				<div className='size-full rounded-md bg-secondary px-4 pb-4'>
 					{children}
